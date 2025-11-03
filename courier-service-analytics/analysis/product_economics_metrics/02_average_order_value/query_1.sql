8b11c23176335984a3cc4f37eab0fadd71969d5f
Angelina Martynova <117866008+angelina-martynova@users.noreply.github.com>
2025-11-04 02:17:23 +0300

    Create query_1.sql

---
+++ courier-service-analytics/analysis/product_economics_metrics/02_average_order_value/query_1.sql
@@ -0,0 +1,8 @@
+-- =========================================================
+-- PROJECT: Courier Service Analytics
+-- TASK: Revenue and Cumulative Growth
+-- GOAL: Calculate daily revenue, cumulative revenue, and
+--       day-over-day revenue growth rate.
+-- =========================================================
+
+WITH successful_orders
