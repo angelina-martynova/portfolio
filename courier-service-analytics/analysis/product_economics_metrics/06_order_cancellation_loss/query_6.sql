629e8c3dae3e54b8fa5bfdad23333ed7bd02e999
Angelina Martynova <117866008+angelina-martynova@users.noreply.github.com>
2025-11-04 03:20:44 +0300

    Create query_6.sql

---
+++ courier-service-analytics/analysis/product_economics_metrics/06_order_cancellation_loss/query_6.sql
@@ -0,0 +1,52 @@
+-- =========================================================
+-- Project: Courier Service Analytics
+-- Query 06: Order Cancellation Loss
+-- Goal: 
+-- =========================================================
+
+WITH successful_orders AS (
+       SELECT creation_time::DATE AS date, 
+              order_id,
+              UNNEST(product_ids) AS product_id
+       FROM orders
+       WHERE order_id NOT IN (
+              SELECT order_id 
+              FROM user_actions 
+              WHERE action='cancel_order'
+       )
+),
+                        
+product_revenues AS (
+       SELECT name AS product_name,
+              SUM(price) AS revenue
+       FROM successful_orders 
+       LEFT JOIN products USING(product_id)
+       GROUP BY name
+),
+
+total_revenue AS (
+       SELECT SUM(price) AS total
+       FROM successful_orders 
+       LEFT JOIN products USING(product_id)
+),
+
+products_with_share AS (
+       SELECT product_name,
+              revenue,
+              ROUND(revenue/(SELECT total FROM total_revenue)*100, 2) AS share_in_revenue
+       FROM product_revenues
+)
+
+SELECT SUM(revenue) AS revenue,
+       CASE
+              WHEN share_in_revenue<0.5 THEN 'ДРУГОЕ'
+              ELSE product_name
+       END AS product_name,
+       ROUND(SUM(share_in_revenue), 2) AS share_in_revenue
+FROM products_with_share
+GROUP BY 
+       CASE
+              WHEN share_in_revenue<0.5 THEN 'ДРУГОЕ'
+              ELSE product_name
+       END 
+ORDER BY revenue DESC
