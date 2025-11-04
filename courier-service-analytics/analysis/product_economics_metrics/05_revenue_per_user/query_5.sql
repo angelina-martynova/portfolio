fb98d27a0385ea79591b94d23ee03e20e16a8167
Angelina Martynova <117866008+angelina-martynova@users.noreply.github.com>
2025-11-04 03:15:38 +0300

    Create query_5.sql

---
+++ courier-service-analytics/analysis/product_economics_metrics/05_revenue_per_user/query_5.sql
@@ -0,0 +1,52 @@
+-- =========================================================
+-- Project: Courier Service Analytics
+-- Query 05: Revenue per User
+-- Goal: 
+-- =========================================================
+
+WITH successful_orders AS (
+       SELECT creation_time::DATE AS date, 
+              order_id,
+              UNNEST(product_ids) AS product_id
+       FROM orders
+       WHERE order_id NOT IN (
+              SELECT order_id 
+              FROM user_actions 
+              WHERE action='cancel_order'
+       )
+),
+
+first_user_actions AS (
+       SELECT user_id,
+              MIN(time::date) AS date
+       FROM user_actions
+       GROUP BY user_id
+),
+
+daily_revenue AS (
+       SELECT date, 
+              SUM(price) AS revenue
+       FROM successful_orders 
+       LEFT JOIN products USING(product_id)
+       GROUP BY date
+),
+
+daily_new_users_revenue AS (
+       SELECT so.date, 
+              SUM(price) AS new_users_revenue
+       FROM successful_orders so
+       LEFT JOIN products USING(product_id)
+       LEFT JOIN user_actions ua USING(order_id)
+       LEFT JOIN first_user_actions fua USING(user_id)
+       WHERE so.date = fua.date AND ua.action != 'cancel_order'
+       GROUP BY so.date
+)
+
+SELECT date, 
+       revenue, 
+       new_users_revenue,
+       ROUND(new_users_revenue/revenue*100, 2) AS new_users_revenue_share,
+       ROUND(((revenue - new_users_revenue)/revenue)*100, 2) AS old_users_revenue_share
+FROM daily_revenue
+LEFT JOIN daily_new_users_revenue USING(date)
+ORDER BY date
